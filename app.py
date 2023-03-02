414ac72b68dae09d71bf9d10501f924ea82475ba
syusuke9999 <shinichi@code-hamamatu.com>
2023-03-02 11:21:51 +0900

    initial commit

--- app.py
+++ app.py
@@ -4,32 +4,52 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = ""
+
+initial = True
+
+prompt: str = """\n"""
+
+query: str = ""
+output_text: str = ""
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    global query, prompt, initial, output_text
+    if initial:
+        initialize()
+        initial = False
+        return redirect(url_for("index"))
     if request.method == "POST":
-        animal = request.form["animal"]
+        query = query + "\n" + request.form["question"]
+        output_text += "\n" + request.form["question"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=query,
+            top_p=1,
+            temperature=0.9,
+            max_tokens=2300,
+            frequency_penalty=0.5,
+            presence_penalty=0,
         )
+        query += "\n" + response.choices[0].text
+        output_text += "\r" + response.choices[0].text
         return redirect(url_for("index", result=response.choices[0].text))
-
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def initialize():
+    global query, prompt
+    query = prompt
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=query,
+        top_p=1,
+        temperature=0.9,
+        max_tokens=2300,
+        frequency_penalty=0.5,
+        presence_penalty=0,
     )
+    return redirect(url_for("index", result=response.choices[0].text))
